4eb28ceadbfa6ae90469ab8e57c19476aa637afd
OMOLLO VANISTELROOY MASIME <111060881+OmolloRovy@users.noreply.github.com>
2025-04-28 21:03:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use('/uploads', express.static('uploads'));
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'V@n!stelr00y',
+  password: '',
   database: 'gallery'
 });
 
